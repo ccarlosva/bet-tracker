36a2dd997ff2d809ebb6284e8603a4e6ca25c7f9
Carlos Varela <cvarela@paystand.com>
2024-02-08 17:30:13 -0600

    fixed some typos

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const { createUser, createLogin,getUser} = require("../controllers/userController")
+const validateToken = require("../middleware/validateTokenHandler")
+const { createUser, createLogin,updateUser} = require("../controllers/userController")
 
 router.route("/register").post(createUser);
 router.route("/login").post(createLogin);
+router.route("/user/update").post(validateToken ,updateUser);
 
 
 module.exports = router
